60acd26874e63d5b260690570711dbff0a7ddcd6
PJunhyuk-mac <jgravity@naver.com>
2017-10-05 00:06:00 +0900

    Change frame2image.py apply def

--- video/frame2image.py
+++ video/frame2image.py
@@ -6,16 +6,22 @@ import math
 import imageio
 from moviepy.editor import *
 
-# Read video from file
+def read_video(video_name):
+    # Read video from file
+    video_name_input = 'testset/' + video_name + '.mov'
+    video = VideoFileClip(video_name_input)
+    return video
+
+def frame2image(video_name):
+    video = read_video(video_name)
+    video_frame = int(video.duration * video.fps) ## duration: second / fps: frame per second
+
+    os.makedirs('testset/' + video_name)
+
+    video_frame_length = math.ceil(math.log(video_frame, 10)) ## ex. 720 -> 3
+
+    for i in range(0, video_frame):
+        video.save_frame('testset/' + video_name + '/frame_' + str(i).zfill(video_frame_length) + '.jpg', i)
+
 video_name = sys.argv[1] ## example: test_video_01
-video_name_input = 'testset/' + video_name + '.mov'
-video_input = VideoFileClip(video_name_input)
-
-video_frame = int(video_input.duration * video_input.fps) ## duration: second / fps: frame per second
-
-os.makedirs('testset/' + video_name)
-
-video_frame_length = math.ceil(math.log(video_frame, 10)) ## ex. 720 -> 3
-
-for i in range(0, video_frame):
-    video_input.save_frame('testset/' + video_name + '/frame_' + str(i).zfill(video_frame_length) + '.jpg', i)
+frame2image(video_name)
