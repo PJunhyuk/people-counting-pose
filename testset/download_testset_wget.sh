68db3a271f17843917f8a614e66ebcbbfab7594a
PJunhyuk-DT <jgravity@naver.com>
2017-10-17 02:19:03 +0900

    Update download_testset_wget.sh

--- testset/download_testset_wget.sh
+++ testset/download_testset_wget.sh
@@ -3,3 +3,7 @@
 wget "http://parkjunhyuk.com/dataset/test_video_01.mov"
 wget "http://parkjunhyuk.com/dataset/test_video_01f.mov"
 wget "http://parkjunhyuk.com/dataset/test_video_02f.mov"
+wget "http://parkjunhyuk.com/dataset/test_video_03f.mov"
+wget "http://parkjunhyuk.com/dataset/test_video_03f.mp4"
+wget "http://parkjunhyuk.com/dataset/test_video_03.mov"
+wget "http://parkjunhyuk.com/dataset/test_video_03_01.mp4"
