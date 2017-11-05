0ab822ad2ffad31d5589b1fe924e0fa51cec9188
PJunhyuk-DT <jgravity@naver.com>
2017-11-06 00:01:52 +0900

    Initialize preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,21 @@
 from moviepy.editor import *
 
-def read_video(video_name):
+
+def read_video(video_file_name):
     # Read video from file
-    video_name_input = 'testset/' + video_name
-    video = VideoFileClip(video_name_input)
-    return video
+    video_file_route = 'testset/' + video_file_name
+    video = VideoFileClip(video_file_route)
+    total_frame_number = int(video.duration * video.fps)
+    return video, total_frame_number
+
+
+def print_video_info(video):
+    print("--Original Video Information--")
+    print("Size: [" + str(video.size[0]) + ", " + str(video.size[1]) + "]")
+    print("Duration(s): " + str(video.duration))
+    print("FPS: " + str(video.fps))
+    print("Total frame number: " + str(video.duration * video.fps))
+
 
 def resize_video(video, video_width):
     # Re-set size of video
@@ -12,7 +23,27 @@ def resize_video(video, video_width):
     print("Changed video size: [" + str(video.size[0]) + ", " + str(video.size[1]) + "]")
     return video
 
-def background_subtraction(video):
 
-if __name__ == '__main__':
-    
+def background_subtraction(video):
+    import cv2
+
+    # TODO
+
+    return video
+
+
+def __init__(self, video_file_name, video_width):
+    video, total_frame_number = read_video(video_file_name)
+    print("read_video complete!")
+
+    print_video_info(video)
+    print("print_video_info complete!")
+
+    if video_width != 0:
+        resize_video(video, video_width)
+        print("resize_video complete!")
+
+    video = background_subtraction(video)
+    print("background_subtraction complete!")
+
+    return video, total_frame_number
--- run.py
+++ run.py
@@ -1,31 +1,39 @@
 import argparse
 
+import time
+time_start = time.time()
+
 import preprocessing
 
 if __name__ == '__main__':
+
+    ## Argument parsing
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', "--videoFile", help="Path to Video File")
+    parser.add_argument('-w', "--videoWidth", help="Width of Output Video")
 
-    ## Preprocessing
-
-    ### Read video from file
     if args["videoFile"] is not None:
-        video_name = args["videoFile"]
+        video_file_name = args["videoFile"]
     else:
         print("You have to input videoFile name")
         sys.exit(1)
-    video = preprocessing.read_video(video_name)
 
-    ### Print informations of video
-    print("Input video size: [" + str(video.size[0]) + ", " + str(video.size[1]) + "]")
-
-    ### Resize video
     if args["videoWidth"] is not None:
         video_width = int(args["videoWidth"])
-        video = preprocessing.resize_video(video, video_width)
+    else:
+        video_width = 0
 
-    ### background subtraction
+    ## Preprocessing
 
-    ## Human detection
+    video, total_frame_number = preprocessing(video_file_name, video_width)
 
-    
+#    for i in range(0, total_frame_number):
+
+        ## Human detection
+
+    ## Poseprocessing
+
+#    poseprocessing(video)
+
+    print("Total time required(s): " + str(time.time() - time_start))
