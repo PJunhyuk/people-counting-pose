7397006c596f565232cb144ed4547d29cd13b2fe
PJunhyuk-mac <jgravity@naver.com>
2017-11-09 00:01:04 +0900

    Add bg_removal to test background subtraction

---
+++ bg_removal.py
@@ -0,0 +1,22 @@
+import cv2
+
+import sys
+import argparse as ap
+
+parser = ap.ArgumentParser()
+parser.add_argument('-f', "--videoFile", help="Path to Video File")
+
+if args["videoFile"] is not None:
+    video_file_name = args["videoFile"]
+else:
+    print("You have to input videoFile name")
+    sys.exit(1)
+
+video_file_route = 'testset/' + video_file_name
+video = cv2.VideoCapture(video_file_route)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+video_name = video_file_name.split('.')[0]
+out = cv2.VideoWriter('testset/' + video_name + '.avi',fourcc, 20.0, (640,480))
