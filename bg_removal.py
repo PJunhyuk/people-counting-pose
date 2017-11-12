3cd4eb6ce507972d3eb6d05533650e2b02d054fe
PJunhyuk-DT <jgravity@naver.com>
2017-11-13 00:18:20 +0900

    Add checking time parts

--- bg_removal.py
+++ bg_removal.py
@@ -1,3 +1,7 @@
+## Check time required
+import time
+time_start = time.time()
+
 ## default
 import sys
 import argparse as ap
@@ -44,4 +48,6 @@ while(1):
 
 video_output = ImageSequenceClip(frame_output_list, fps=video_fps)
 video_name = video_file_name.split('.')[0]
-video_output.write_videofile("testset/" + video_name + "_bgrm.mp4", fps=video.fps, progress_bar=False)
+video_output.write_videofile("testset/" + video_name + "_bgrm.mp4", fps=video_fps, progress_bar=False)
+
+print("Total time required(s): " + str(time.time() - time_start))
