ed7c75bbfcc9f547fd5d3555c4d2c7a31f941daa
MCML-PJunhyuk-LabDT <jgravity@naver.com>
2018-06-21 17:16:03 +0900

    Fix link url in download_testset_wget

--- testset/download_testset_wget.sh
+++ testset/download_testset_wget.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-wget "http://jgravity96.cafe24.com/dataset/test_video_01.mov"
-wget "http://jgravity96.cafe24.com/dataset/test_video_01f.mov"
-wget "http://jgravity96.cafe24.com/dataset/test_video_02f.mov"
-wget "http://jgravity96.cafe24.com/dataset/test_video_03f.mov"
-wget "http://jgravity96.cafe24.com/dataset/test_video_03f.mp4"
-wget "http://jgravity96.cafe24.com/dataset/test_video_03.mov"
-wget "http://jgravity96.cafe24.com/dataset/test_video_03_01.mp4"
+wget "http://jgravity0.cafe24.com/dataset/test_video_01.mov"
+wget "http://jgravity0.cafe24.com/dataset/test_video_01f.mov"
+wget "http://jgravity0.cafe24.com/dataset/test_video_02f.mov"
+wget "http://jgravity0.cafe24.com/dataset/test_video_03f.mov"
+wget "http://jgravity0.cafe24.com/dataset/test_video_03f.mp4"
+wget "http://jgravity0.cafe24.com/dataset/test_video_03.mov"
+wget "http://jgravity0.cafe24.com/dataset/test_video_03_01.mp4"
